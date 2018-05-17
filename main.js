2b38b79f459bc5cabd6f2a222a1dbc6a679b3191
Melvin Hicklin <melvinhicklin@gmail.com>
2018-05-17 00:50:47 -0700

    Update main.js
    quick change to timing loop.  From 1500 to 500.

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ create: function() {
         spaceKey.onDown.add(this.jump, this);
 
     this.pipes = game.add.group();
-    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    this.timer = game.time.events.loop(500, this.addRowOfPipes, this);
 },
 
 update: function() {
